b0de233909586673d3252261e672450cbdd4a6a3
juanmanuelpico2 <picojuanm@gmail.com>
2024-04-15 19:57:00 -0300

    primer commit

---
+++ AulasApp/src/main/java/com/equipo6/aulasUnla/AulasUnlaApplication.java
@@ -0,0 +1,13 @@
+package com.equipo6.aulasUnla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasUnlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasUnlaApplication.class, args);
+	}
+
+}
---
+++ AulasApp/src/test/java/com/equipo6/aulasUnla/AulasUnlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo6.aulasUnla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasUnlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
