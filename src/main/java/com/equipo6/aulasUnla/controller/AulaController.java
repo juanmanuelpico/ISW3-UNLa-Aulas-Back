ea38314b64c10d56013753ae1ff618230ef9b046
juanmanuelpico <picojuan322@gmail.com>
2024-04-22 16:18:16 -0300

    se actualiza endpoint de desasignar como delelte

--- src/main/java/com/equipo6/aulasUnla/controller/AulaController.java
+++ src/main/java/com/equipo6/aulasUnla/controller/AulaController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,7 +50,7 @@ public class AulaController {
         }
     }
 
-    @PutMapping("/desasignarMateriasDeAulas")
+    @DeleteMapping("/desasignarMateriasDeAulas")
     public ResponseEntity<Object> desasiganarMateriasDeAulas(){
         try {
             aulaService.desasignarTodasMaterias();
