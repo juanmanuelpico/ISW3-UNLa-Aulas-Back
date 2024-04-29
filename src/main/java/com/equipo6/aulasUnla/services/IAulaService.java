c78a327755bfaa3c068307ca3e6c47a2eb383721
lassefh <semtra17@gmail.com>
2024-04-29 13:46:36 -0300

    rama_juan mergeado con rama_Dev

--- src/main/java/com/equipo6/aulasUnla/services/IAulaService.java
+++ src/main/java/com/equipo6/aulasUnla/services/IAulaService.java
@@ -2,14 +2,14 @@ package com.equipo6.aulasUnla.services;
 
 import java.util.List;
 
-import com.equipo6.aulasUnla.dtos.request.AulaDTORequest;
 import com.equipo6.aulasUnla.dtos.response.AulaDTOResponse;
+import com.equipo6.aulasUnla.entities.Materia;
 
 public interface IAulaService {
     
     public List<AulaDTOResponse> obtenerListadoAulas(int cantEstudiantes, String turno) throws Exception;
 
-    public boolean asignarMateriaAAula(int idAula, String nombreMateria) throws Exception;
+    public Materia asignarMateriaAAula(int idAula, String nombreMateria) throws Exception;
 
     public boolean desasignarTodasMaterias() throws Exception;
 
